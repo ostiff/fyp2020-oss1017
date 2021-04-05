faf850aed1d1343f4dd4ef505ccae5dba47921ed
ostiff <37678137+ostiff@users.noreply.github.com>
2021-04-05 12:54:52 +0100

    Add: vae class and helper functions [UNTESTED]

--- pkgname/utils/data_loader.py
+++ pkgname/utils/data_loader.py
@@ -1,6 +1,7 @@
 #TODO: include scaling?
 
 import pandas as pd
+import numpy as np
 from dataprep.eda import create_report
 
 from definitions import DENGUE_PATH, PATHOLOGY_PATH
@@ -91,5 +92,40 @@ def report_pathology(path=PATHOLOGY_PATH, dropna=False, usecols=None, usedefault
 if __name__ == "__main__":
     #report_pathology(usedefault=True, dropna=True, removecorr=0.9)
     #report_dengue(usedefault=True, dropna=False)
+    #df = load_dengue(usedefault=True)
+
     df = load_dengue(usedefault=True)
 
+    mapping = {'Female': 1, 'Male': 2, True: 1, False: 0}
+    df = df.replace({'gender': mapping, 'bleeding': mapping, 'shock': mapping,
+                     'bleeding_gum': mapping, 'abdominal_pain': mapping, 'ascites': mapping,
+                     'bleeding_mucosal': mapping, 'bleeding_skin': mapping})
+
+    features = ["date", "age", "gender", "weight", "bleeding", "plt",
+                "shock", "haematocrit_percent", "bleeding_gum", "abdominal_pain",
+                "ascites", "bleeding_mucosal", "bleeding_skin", "body_temperature"]
+
+    for feat in features:
+        df[feat] = df.groupby('study_no')[feat].ffill().bfill()
+
+    df = df.loc[df['age'] <= 18]
+    df = df.dropna()
+
+    df = df.groupby(by="study_no", dropna=False).agg(
+        date=pd.NamedAgg(column="date", aggfunc="last"),
+        age=pd.NamedAgg(column="age", aggfunc="max"),
+        gender=pd.NamedAgg(column="gender", aggfunc="first"),
+        weight=pd.NamedAgg(column="weight", aggfunc=np.mean),
+        bleeding=pd.NamedAgg(column="bleeding", aggfunc="max"),
+        plt=pd.NamedAgg(column="plt", aggfunc="max"),
+        shock=pd.NamedAgg(column="shock", aggfunc="max"),
+        haematocrit_percent=pd.NamedAgg(column="haematocrit_percent", aggfunc="max"),
+        bleeding_gum=pd.NamedAgg(column="bleeding_gum", aggfunc="max"),
+        abdominal_pain=pd.NamedAgg(column="abdominal_pain", aggfunc="max"),
+        ascites=pd.NamedAgg(column="ascites", aggfunc="max"),
+        bleeding_mucosal=pd.NamedAgg(column="bleeding_mucosal", aggfunc="max"),
+        bleeding_skin=pd.NamedAgg(column="bleeding_skin", aggfunc="max"),
+        body_temperature=pd.NamedAgg(column="body_temperature", aggfunc=np.mean),
+    ).dropna()
+
+    df.to_csv('dengue.csv')
