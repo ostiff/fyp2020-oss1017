486b7174d480079d155eaade4bb052154c4bed54
ostiff <37678137+ostiff@users.noreply.github.com>
2021-04-08 14:55:12 +0100

    Update: conf for plotly, vae example

--- examples/vae/plot_vae_dengue_clustering_1.py
+++ examples/vae/plot_vae_dengue_clustering_1.py
@@ -1,3 +1,12 @@
+"""
+Beta-VAE used for clustering
+============================
+
+Type of Variational Auto-Encoder where the KL divergence term in the loss
+function is weighted by a parameter `beta`.
+
+"""
+
 # %%
 # Imports and reproducibility
 # ---------------------------
@@ -5,6 +14,7 @@
 # Set seed for reproducibility.
 
 import os
+import warnings
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader
@@ -12,6 +22,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 from definitions import DATA_DIR
 from pkgname.core.VAE.vae import VAE, train_vae, plot_vae_loss, get_device, set_seed
@@ -28,7 +40,7 @@ device = get_device(False)
 # Hyperparameters
 # ---------------
 
-num_epochs = 50
+num_epochs = 20
 learning_rate = 0.0001
 batch_size = 16
 latent_dim = 2
@@ -94,7 +106,7 @@ plt.scatter(encoded_test[:, 0], encoded_test[:, 1])
 plt.show()
 
 # Perform clustering on encoded inputs
-cluster = KMeans(n_clusters=8, random_state=SEED).fit_predict(encoded_test)
+cluster = KMeans(n_clusters=3, random_state=SEED).fit_predict(encoded_test)
 plt.scatter(encoded_test[:, 0], encoded_test[:, 1], c=cluster)
 plt.show()
 
@@ -102,26 +114,50 @@ plt.show()
 # %%
 # Cluster analysis
 # ----------------
+#
+# These attributes were not used to train the model.
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
 
-test_info['cluster'] = cluster
+    test_info['cluster'] = cluster
 
-_, ax0 = plt.subplots(3, 1, figsize=(5, 15))
-test_info.boxplot('age', 'cluster', ax=ax0[0], showmeans=True)
-test_info.boxplot('gender', 'cluster', ax=ax0[1], showmeans=True)
-test_info.boxplot('weight', 'cluster', ax=ax0[2], showmeans=True)
-plt.show()
+    fig = make_subplots(rows=3, cols=1, vertical_spacing=0.05)
 
+    for i, feat in enumerate(['age','gender','weight']):
+        fig.add_trace(
+            go.Box(x=test_info["cluster"].values, y=test_info[feat].values,
+                   boxpoints='outliers', boxmean=True),
+            row=i + 1, col=1
+        )
+        fig.update_xaxes(title_text="Cluster", row=i + 1, col=1)
+        fig.update_yaxes(title_text=feat, row=i + 1, col=1)
 
-test_data['cluster'] = cluster
+    fig.update_layout(height=1800, title_text="Attributes not used in training", showlegend=False)
+fig
 
-_, ax1 = plt.subplots(5, 2, figsize=(10, 25))
+#%%
+# The following attributes were used to train the model.
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
 
-features = ["bleeding", "plt", "shock", "haematocrit_percent", "bleeding_gum",
-            "abdominal_pain", "ascites", "bleeding_mucosal", "bleeding_skin",
-            "body_temperature"]
+    test_data['cluster'] = cluster
 
-for i, feat in enumerate(features):
-    test_data.boxplot(feat, 'cluster', ax=ax1[i % 5][i % 2], showmeans=True)
+    features = ["bleeding", "plt", "shock", "haematocrit_percent", "bleeding_gum",
+                "abdominal_pain", "ascites", "bleeding_mucosal", "bleeding_skin",
+                "body_temperature"]
 
-plt.show()
+    cols = 2
+    rows = 5
+    fig = make_subplots(rows=rows, cols=cols, vertical_spacing=0.05, horizontal_spacing= 0.2)
 
+    for i, feat in enumerate(features):
+        fig.add_trace(
+            go.Box(x=test_data["cluster"].values, y=test_data[feat].values,
+                   boxpoints='outliers', boxmean=True),
+            row=(i // cols) + 1, col=(i % cols) + 1
+        )
+        fig.update_xaxes(title_text="Cluster", row=(i // cols) + 1, col=(i % cols) + 1)
+        fig.update_yaxes(title_text=feat, row=(i // cols) + 1, col=(i % cols) + 1)
+
+    fig.update_layout(height=3000, title_text="Attributes used in training", showlegend=False)
+fig
