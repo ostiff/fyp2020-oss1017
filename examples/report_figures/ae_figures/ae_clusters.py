5d01325a40875c6c19828d8bcbb2a0b6a09ca9a4
ostiff <37678137+ostiff@users.noreply.github.com>
2021-07-02 12:51:58 +0100

    Update: AE figures

--- examples/report_figures/ae_figures/ae_clusters.py
+++ examples/report_figures/ae_figures/ae_clusters.py
@@ -1,3 +1,8 @@
+"""
+Autoencoder experiment figures
+==============================
+
+"""
 import os
 import sys
 import pickle
@@ -11,6 +16,7 @@ import matplotlib.patches as mpatches
 from sklearn import preprocessing, mixture
 from seaborn import color_palette
 from sklearn.model_selection import train_test_split
+from tableone import TableOne
 from torch.utils.data import DataLoader
 
 from core.evaluation.dr_evaluation import distance_metrics
@@ -18,24 +24,24 @@ from pkgname.utils.data_loader import load_dengue, IQR_rule
 from definitions import ROOT_DIR
 from sklearn.cluster import DBSCAN, KMeans
 
-from utils.plot_utils import plot_results
+from utils.plot_utils import plot_results, formatTable
 
 sys.path.insert(0, os.path.abspath('.'))
 
-mpl.use("pgf")
+# mpl.use("pgf")
 mpl.rcParams.update({
-    "pgf.texsystem": "pdflatex",
-    'font.family': 'serif',
-    # 'font.weight': 'light',
-    # 'font.size': 16,
-
-
-    'text.usetex': True,
-    'font.size': 18,
+    # "pgf.texsystem": "pdflatex",
+    # 'font.family': 'serif',
+    'font.weight': 'light',
+    'font.size': 16,
+    #
+    #
+    # 'text.usetex': True,
+    # 'font.size': 18,
 })
 
 SEED = 0
-MODEL_PATH = os.path.join(ROOT_DIR, 'examples', 'report_figures', 'ae_figures', 'model')
+MODEL_PATH = os.path.join(ROOT_DIR, 'examples', 'report_figures', 'ae_figures', 'ae_sig_3')
 np.random.seed(SEED)
 
 
@@ -86,20 +92,6 @@ info_feat = ["shock", "bleeding", "bleeding_gum", "abdominal_pain", "ascites",
              "bleeding_mucosal", "bleeding_skin", "gender"]
 data_feat = ["age", "weight", "plt", "haematocrit_percent", "body_temperature"]
 
-# train, test = train_test_split(df, test_size=0.2, random_state=SEED)
-#
-# train_data = train[data_feat]
-# test_data = test[data_feat]
-# train_info = train[info_feat]
-# test_info = test[info_feat]
-#
-# scaler = preprocessing.MinMaxScaler().fit(train_data)
-#
-# train_scaled = scaler.transform(train_data.to_numpy())
-# test_scaled = scaler.transform(test_data.to_numpy())
-# loader_train = DataLoader(train_scaled, batch_size, shuffle=False)
-# loader_test = DataLoader(test_scaled, batch_size, shuffle=False)
-
 info_feat = ["shock", "bleeding", "bleeding_gum", "abdominal_pain", "ascites",
              "bleeding_mucosal", "bleeding_skin", "gender"]
 data_feat = ["age", "weight", "plt", "haematocrit_percent", "body_temperature"]
@@ -132,14 +124,10 @@ for i in [3,12,15]:
 gmm = mixture.GaussianMixture(n_components=3,
                                         covariance_type='full', random_state=SEED).fit(ae_points)
 clusters = gmm.predict(ae_points)
-# clusters_k_means = KMeans(n_clusters=3, random_state=SEED).fit_predict(ae_points)
+clusters_k_means = KMeans(n_clusters=3, random_state=SEED).fit_predict(ae_points)
 
 
 before_mapping['cluster'] = clusters
-before_mapping['cluster'] = before_mapping['cluster'].replace([1, 2], [2, 1])
-
-print(before_mapping[(before_mapping.shock > 0) & (before_mapping.cluster == 1)].shape[0])
-print(before_mapping[before_mapping.cluster == 1].shape[0])
 
 # %%
 # Plotting
@@ -159,9 +147,9 @@ for feat in sorted(data_feat):
     axes[i].set(xlabel=label_mapping[feat], ylabel=None)
 
     if i == 2:
-        c1 = mpatches.Patch(color=colours[0], label='Cluster 1')
-        c2 = mpatches.Patch(color=colours[1], label='Cluster 2')
-        c3 = mpatches.Patch(color=colours[2], label='Cluster 3')
+        c1 = mpatches.Patch(color=colours[0], label='Cluster 0')
+        c2 = mpatches.Patch(color=colours[1], label='Cluster 1')
+        c3 = mpatches.Patch(color=colours[2], label='Cluster 2')
 
         axes[3].legend(handles=[c1, c2, c3], loc='center')
         i += 1
@@ -188,9 +176,12 @@ for feat in sorted(info_feat):
 
     i += 1 if i != 11 else 2
 
-fig.savefig("ae_cluster_stats.pdf", bbox_inches='tight')
+# fig.savefig("ae_cluster_stats.pdf", bbox_inches='tight')
+plt.show()
 
-# SHOCK CLUSTER COMP
+# %%
+# Intra-cluster comparison
+# ------------------------
 fig, axes = plt.subplots(1, 6, figsize=(15, 4))
 fig.tight_layout()
 axes=axes.flatten()
@@ -201,7 +192,7 @@ for ax in axes:
 
 for i, feat in enumerate(sorted(data_feat)):
     g = sns.boxplot(ax=axes[i], x="cluster", y=feat,
-                     data=before_mapping[before_mapping['cluster'] == 2], hue='shock', palette="Set3")
+                     data=before_mapping[before_mapping['cluster'] == 1], hue='shock', palette="Set3")
     axes[i].get_xaxis().set_ticks([])
     axes[i].set(xlabel=label_mapping[feat], ylabel=None)
     g.legend_.remove()
@@ -213,12 +204,14 @@ c1 = mpatches.Patch(color=legend_colours[0], label='False')
 c2 = mpatches.Patch(color=legend_colours[1], label='True')
 axes[-1].legend(handles=[c1, c2], loc='center', title='Shock')
 
-fig.savefig("ae_cluster0_shock.pdf", bbox_inches='tight')
+# fig.savefig("ae_cluster0_shock.pdf", bbox_inches='tight')
+plt.show()
 
 
 
-
-#GMM
+# %%
+# GMM clustering
+# --------------
 colours = np.array(sns.color_palette("pastel", as_cmap=True))
 # colours = np.insert(colours, 0, "#737373")
 colours = dict(zip(list(range(len(colours))), colours))
@@ -228,35 +221,35 @@ ind_list = np.random.choice(len(df.index), 5000)
 ae_points_subset = np.take(ae_points, ind_list, axis=0)
 clusters = np.take(clusters, ind_list, axis=0)
 
-
-
 plot_results(ae_points_subset, clusters, gmm.means_, gmm.covariances_, colours, labels)
+# plt.savefig("ae_gmm.pdf", bbox_inches='tight')
+plt.show()
 
-# scatter = sns.scatterplot(x=ae_points[:, 0], y=ae_points[:, 1],
-#                           hue=clusters, palette=colours, linewidth=0,
-#                           s=10)
-# handles, _  =  scatter.get_legend_handles_labels()
-#
-# scatter.legend(handles, labels, loc='lower right', borderpad=0.2,labelspacing=0.2)
-plt.savefig("ae_gmm.pdf", bbox_inches='tight')
-# plt.show()
+# %%
+# k-means clustering
+# ------------------
 
-#KMEANS
-
-# clusters_k_means = np.take(clusters_k_means, ind_list, axis=0)
-# plt.figure()
-# splot = sns.scatterplot(x=ae_points_subset[:, 0], y=ae_points_subset[:, 1], hue=clusters_k_means, palette=colours,
-#                             linewidth=0, s=10)
-# handles, _ = splot.get_legend_handles_labels()
-# splot.legend(handles, labels, loc='lower right', borderpad=0.2, labelspacing=0.2)
+clusters_k_means = np.take(clusters_k_means, ind_list, axis=0)
+plt.figure()
+splot = sns.scatterplot(x=ae_points_subset[:, 0], y=ae_points_subset[:, 1], hue=clusters_k_means, palette=colours,
+                            linewidth=0, s=10)
+handles, _ = splot.get_legend_handles_labels()
+splot.legend(handles, labels, loc='lower right', borderpad=0.2, labelspacing=0.2)
 # plt.savefig("ae_kmeans.pdf", bbox_inches='tight')
+plt.show()
 
 
-#SHEPPARD
-# _, fig = distance_metrics(scaled, ae_points, 6000, '', verbose=False)
+# %%
+# AE Sheppard Diagram
+# -------------------
+_, fig = distance_metrics(scaled, ae_points, 6000, '', verbose=False)
 # fig.savefig("ae_sheppard.png", bbox_inches='tight', dpi=300)
+plt.show()
 
-# SHOCK LABEL
+
+# %%
+# Shock in latent dimension
+# -------------------------
 shock = np.take(before_mapping['shock'].to_numpy(), ind_list, axis=0)
 plt.figure()
 splot = sns.scatterplot(x=ae_points_subset[:, 0], y=ae_points_subset[:, 1], hue=shock, palette='pastel',
@@ -264,6 +257,5 @@ splot = sns.scatterplot(x=ae_points_subset[:, 0], y=ae_points_subset[:, 1], hue=
 legend_colours = np.array(color_palette('pastel').as_hex())
 
 plt.legend(title='Shock', loc='lower right',borderpad=0.2,labelspacing=0.2)
-
-
-plt.savefig("ae_sig_shock.pdf", bbox_inches='tight')
+# plt.savefig("ae_sig_shock.pdf", bbox_inches='tight')
+plt.show()
